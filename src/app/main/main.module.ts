bb614fb24a7e2c63bbbe0606e469fc6bd75c2e45
libra307 <34910844+libra307@users.noreply.github.com>
2017-12-31 20:17:54 +0700

    form main

--- src/app/main/main.module.ts
+++ src/app/main/main.module.ts
@@ -3,12 +3,14 @@ import { CommonModule } from '@angular/common';
 import { MainComponent } from './main.component';
 import { mainRoutes } from './main.routes';
 import { RouterModule, Route } from '@angular/router'
-import { UserModule } from './user/user.module';
+//import { UserModule } from './user/user.module';
+//import { HomeModule } from './home/home.module';
 
 @NgModule({
   imports: [
     CommonModule,
-    UserModule,
+   // UserModule,
+    //HomeModule,
     RouterModule.forChild(mainRoutes)
 
   ],
