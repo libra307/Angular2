a008c404ba15f9a3bcfcf46c446a4720465ec110
libra307 <34910844+libra307@users.noreply.github.com>
2017-12-31 21:31:33 +0700

    services part 1

---
+++ src/app/core/common/message.constants.ts
@@ -0,0 +1,8 @@
+export class MessageConstants{
+    public static SYSTEM_ERROR_MSG = "Có lỗi kết nối đến máy chủ";
+    public static CONFIRM_DELETE_MSG = "Bạn có chắc chắn muốn xóa bản ghi này";
+    public static LOGIN_AGAIN_MSG = "Bạn hết phiên đăng nhập mời đăng nhập lại";
+    public static CREATED_OK_MSG = "thêm mới thành công";
+    public static UPDATED_OK_MSG = "Đã cập nhật xong";
+    public static DELETED_OK_MSG = "Đã xóa xong";
+}
---
+++ src/app/core/servies/data.service.ts
@@ -0,0 +1,19 @@
+import { Injectable } from '@angular/core';
+
+@Injectable()
+export class DataService {
+
+  constructor() { }
+get (uri:string){
+  return null;
+}
+post(uri:string,data?:any){
+  return  null;
+}
+put(uri:string,data?:any){
+return  null;
+}
+postfile(uri:string,data?:any){
+
+}
+}
